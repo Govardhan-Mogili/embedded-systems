ae724ee5f258befc1b77a6a770710b841e9963c3
Govardhan-Mogili <80757825+Govardhan-Mogili@users.noreply.github.com>
2021-04-29 11:17:16 +0530

    Add files via upload

---
+++ inc/activity1.h
@@ -0,0 +1,10 @@
+#ifndef _Activity_1_
+#define _Activity_1_
+
+
+#include<avr/io.h>
+
+void activity1_init();
+void activity_loop();
+
+#endif
---
+++ project_main.c
@@ -0,0 +1,7 @@
+#include "activity1.h"
+int main(void){
+activity1_init();
+while(1)
+	activity1_loop();
+return 0;
+}
