a0a8e1c881a9813fdce93025c3a0d645679c0798
Govardhan-Mogili <80757825+Govardhan-Mogili@users.noreply.github.com>
2021-04-30 17:28:00 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,5 +1,6 @@
 #include "act2.h"
 #include"activity1.h"
+#include"act3.h"
 uint16_t temp;
 int main(void){
 activity1_init();
@@ -8,6 +9,7 @@ while(1){
 	activity1_loop();
 	temp=ReadADC(3);
 	_delay_ms(200);
+	OutPWM(temp);
 }
 return 0;
 }
