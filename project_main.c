3597c6794f1230acfcd775d83b73ce8ef8ea6072
Govardhan-Mogili <80757825+Govardhan-Mogili@users.noreply.github.com>
2021-04-30 19:07:48 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -5,19 +5,19 @@ uint16_t temp;
 char temp_data;
 void peripheral_init(void)
 {
+	activity1_init();
 	InitADC();
 	InitPWM();
 	InitUART(103);
 int main(void){
 	peripheral_init();
-activity1_init();
-InitADC();
-void InitPWM();
+
 while(1){
 	activity1_loop();
 	temp=ReadADC(3);
 	_delay_ms(200);
-	OutPWM(temp);
+	temp_data= OutPWM(temp);
+	UARTwrite(temp_data);
 }
 return 0;
 }
